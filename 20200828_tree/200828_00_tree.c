444f013ef7418113d2baf7bd27f690eb96c8c22a
moosongsong <50647845+moosongsong@users.noreply.github.com>
2020-08-28 10:37:47 +0900

    update 0828

---
+++ 20200828_tree/200828_00_tree.c
@@ -0,0 +1,14 @@
+//
+// Created by songe on 2020-08-27.
+//
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    freopen("C:\\project\\Algorithm_in_C\\20200828_tree\\200828_00_data.txt", "r", stdin);
+    int promise, out, count;
+    scanf("%d %d %d", &promise, &out, &count);
+
+
+    printf("%d %d %d", promise, out, count);
+    return 0;
+}
